cf6ed37efdaa1cc864ac4956a55f1b7fae76f121
Ahmet Faruk şengül <68163224+lightlessy@users.noreply.github.com>
2026-07-28 23:49:16 +0300

    Use calibrated detector in debug script

--- debug.bat
+++ debug.bat
@@ -5,5 +5,5 @@ if not exist ".venv\Scripts\python.exe" (
   pause
   exit /b 1
 )
-".venv\Scripts\python.exe" spotify_snap_bounded.py --debug
+".venv\Scripts\python.exe" calibrated_detector.py --debug
 pause
