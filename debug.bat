3cbab1d78445e79aadad3919e418bedc9aa97096
Ahmet Faruk şengül <68163224+lightlessy@users.noreply.github.com>
2026-07-28 20:54:18 +0300

    Debug feedback-enabled controller

--- debug.bat
+++ debug.bat
@@ -5,5 +5,5 @@ if not exist ".venv\Scripts\python.exe" (
   pause
   exit /b 1
 )
-".venv\Scripts\python.exe" spotify_snap.py --debug
+".venv\Scripts\python.exe" spotify_snap_feedback.py --debug
 pause
